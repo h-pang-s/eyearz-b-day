b9aefd7a479fa490318cb7bd172a10bba63308f3
h-pang-s <rhkd0884@naver.com>
2025-05-16 02:09:32 +0900

    Commit

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./public', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
